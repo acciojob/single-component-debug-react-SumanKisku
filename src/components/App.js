b15ebdfbcbab0cd380b261d1406ab35dbfe32e56
Suman Kisku <sumankisku1@gmail.com>
2023-03-11 00:12:23 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
 
 function App() {
     return (
-        <>
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-        </>
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     )
 }
 
